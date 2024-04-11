13672ce23ef514a4b44bf34f31d32e41889b1765
kapilnagar72 <166583529+kapilnagar72@users.noreply.github.com>
2024-04-11 19:17:26 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+
+//What is the difference between length and length() method Give an Examples?
+
+public class Test {
+    public static void main(String args[]) {
+        int array[] = { 1, 2, 3, 4, 5, 6, 7 };
+        System.out.println("Length of an array is: " + array.length);
+    }
+}
+
---
+++ Test1.java
@@ -0,0 +1,9 @@
+
+// What is the difference between length and length() method Give an Examples?
+
+public class Test1 {
+public static void main(String args[]) {
+String str = "Welcome to Tutorials Point";
+System.out.println("Length of String using length() method is:" + str.length());
+}
+}
